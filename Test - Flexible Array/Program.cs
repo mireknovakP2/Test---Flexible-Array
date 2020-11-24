32305141622c18db6bb2a78e5855651838873231
Miroslav Novák <u19.417@pslib.cloud>
2020-11-24 08:17:42 +0100

    Přidejte soubory projektu.

---
+++ Test - Flexible Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test___Flexible_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
